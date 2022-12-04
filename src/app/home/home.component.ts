b15a7ef89805f1497dc030efd9938bdac304751f
Adinortey Lawerteh <adinortey360@gmail.com>
2022-12-04 01:37:02 +0000

    Exchange rate api integration

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -22,8 +22,46 @@ export class HomeComponent implements OnInit {
     this.currencies.push({});
   }
 
-  public calculateTotal(): void {
-    // Calculate the total value of all currencies in the target currency
-  }
+  public async calculateTotal(): Promise<void> {
+    let total: number = 0;
+  
+    // Loop over the currencies and convert each one to the target currency
+    for (const currency of this.currencies) {
+      //get index
+      const index = this.currencies.indexOf(currency);
+      //input field of value is id=currency-
+      //input field of value is id=currency-index, currency field of value is currency-, currency field of value is currency-{{ indexOfelement }}-type: get values by id and put in variables
+      const amount = parseFloat((document.getElementById(`currency-${index}`) as HTMLInputElement).value);
+      const currencytype = (document.getElementById(`currency-${index}-type`) as HTMLInputElement).value;
 
+      // Convert the currency to the target currency
+      const convertedCurrency = this.convertCurrency(amount, currencytype, this.targetCurrency);
+  
+      // Add the converted currency to the total
+      // Add the converted currency to the total
+      total += await convertedCurrency;
+    }
+  
+    // Display the total
+    console.log(total);
+  }
+ 
+  
+  public async convertCurrency(amount: number, from: string, to: string): Promise<number> {
+    // Call a currency conversion API to get the conversion rate
+    const conversionRate = this.getConversionRate(from, to);
+  
+    // Convert the currency using the conversion rate
+    return amount * await conversionRate;
+  }
+  
+  public async getConversionRate(from: string, to: string): Promise<number> {
+    // Use the Open Exchange Rates API to get the conversion rate
+    const response = fetch(`https://openexchangerates.org/api/latest.json?app_id=&base=${from}&symbols=${to}`);
+    const json = (await response).json();
+  
+    return json.then((data: any) => {
+      return data.rates[to];
+    });
+  }
 }
