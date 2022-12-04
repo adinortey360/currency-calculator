72bfffcf3e8e77cfa46ec6a2e43ac7515249db01
Adinortey Lawerteh <adinortey360@gmail.com>
2022-12-04 20:43:21 +0000

    default window size

--- electron.js
+++ electron.js
@@ -2,7 +2,7 @@ const { app, BrowserWindow } = require('electron');
 
 
 function createWindow() {
-    win = new BrowserWindow({width: 800, height: 800});
+    win = new BrowserWindow({width: 850, height: 850});
     win.loadFile('dist/currency/index.html');
 }
 
